347bbf91a9805ce161334251ea365cac8f1dd2c0
wayoalamos <malamos2@uc.cl>
2018-11-19 02:14:11 -0300

    added reducer, provider, store, createStore

---
+++ manager/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import reducers from './reducers'
+
+class App extends Component {
+  render() {
+    console.log('logeandoperro!')
+    return (
+      <Provider store={createStore(reducers)}>
+        <View>
+          <Text>Tomy chuṕa el pico</Text>
+        </View>
+      </Provider>
+    )
+  }
+}
+
+export default App
